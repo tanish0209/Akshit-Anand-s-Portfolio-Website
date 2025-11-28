e4c59d01df0beb74b57a0ec94840cf0794b62dd4
Tanish <bajajtanish0209@gmail.com>
2025-11-28 19:56:43 +0530

    Completed About Page sections-certifications,memberships

--- components/About/Certifications.jsx
+++ components/About/Certifications.jsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+const certifications = [
+  "Certified VitalStim Therapist, Envois, USA - 2021",
+  "Certified Laryngeal Voice Restoration Expert - Tata Memorial Hospital, Mumbai - 2022 ",
+  " Certified for Fibre optic Endoscopic Evaluation of Swallowing - SLPQuest Bangalore - 2023",
+  " Certified for Videofluoroscopy Swallow Study - Tata Memorial Hospital, Mumbai -2024",
+  "Manual Therapy Expert - Waltz Fritz, USA -2025",
+];
 function Certifications() {
-  return <div>Certifications</div>;
+  return (
+    <div className="p-15 border-b border-b-black/10">
+      <h2 className="text-4xl mb-10">Certifications</h2>
+
+      <div className="grid grid-cols-2 gap-8 max-w-6xl place-self-center">
+        {certifications.map((item, index) => (
+          <div
+            key={item}
+            className={`bg-white border border-[#B3C8E1] highlight-text text-2xl hover:scale-105 duration-300 px-10 py-4 rounded-full w-full max-w-132 place-content-center text-center mx-auto
+        ${index === certifications.length - 1 ? "col-span-2" : ""}
+      `}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Certifications;
--- components/About/Memberships.jsx
+++ components/About/Memberships.jsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+const memberships = [
+  "Indian Speech and Hearing Association (ISHA)",
+  "Delhi Branch of Indian Speech and Hearing Association (DISHA)",
+  "SLPQuest, Bangalore, India",
+  "Rehabilitation Council Of India (RCI) - A78605",
+];
 function Memberships() {
-  return <div>Memberships</div>;
+  return (
+    <div className="p-15 border-b border-b-black/10">
+      <h2 className="text-4xl mb-10">Memberships</h2>
+      <div>
+        {memberships.map((item, index) => (
+          <div
+            key={item}
+            className={`py-2  highlight-text text-2xl border-t border-t-black/40 ${
+              index == memberships.length - 1
+                ? " border-b border-b-black/40"
+                : ""
+            }`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Memberships;
