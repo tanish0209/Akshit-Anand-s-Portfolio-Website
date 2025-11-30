b6d25937713273b0a5345c2b6c026b546f8bb0e3
Tanish <bajajtanish0209@gmail.com>
2025-11-30 14:37:45 +0530

    updated sidebar z-index

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -133,7 +133,7 @@ export default function Navbar() {
 
       {/* Sidebar */}
       <aside
-        className={`fixed top-0 right-0 h-full w-80 bg-white shadow-2xl z-50 transform transition-transform duration-300 ease-in-out xl:hidden ${
+        className={`fixed top-0 right-0 h-full w-80 bg-white shadow-2xl z-550 transform transition-transform duration-300 ease-in-out xl:hidden ${
           isSidebarOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
